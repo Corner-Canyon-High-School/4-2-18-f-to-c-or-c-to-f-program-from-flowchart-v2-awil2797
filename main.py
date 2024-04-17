b437fead35656e6def1db8ac61a0217d23d8667d
awil2797 <162728967+awil2797@users.noreply.github.com>
2024-04-17 16:31:29 +0000

    submit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+temp =int(input("whats the temperature? "))
+choice = int(input("type 1 for fahenheit-to-centigrade. type 2 for centigrade-to-farenheit. "))
+if choice == 1:
+    print((9*temp + (32 * 5))/5)
+elif choice == 2: 
+    print((5/9) * (temp - 32))
